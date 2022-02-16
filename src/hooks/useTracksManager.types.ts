a7ffd73a679a256c60a2ed7b45934ad2b7b1925d
Gwenaël Girod <gwenael.girod@gmail.com>
2022-02-16 18:46:24 +0100

    fix: better threejs performance + title animations + better static effect + author link

--- src/hooks/useTracksManager.types.ts
+++ src/hooks/useTracksManager.types.ts
@@ -3,5 +3,6 @@ import { ITrack } from '../typings/Tracks.types';
 export interface IUseTracksManager {
   track?: ITrack;
   nextTrack: () => ITrack | null;
+  previousTrackTitle: string;
   checkTrackWithTimestamp: (timestamp: number) => void;
 }
